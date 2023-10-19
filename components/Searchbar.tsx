e195c260855ceeccf91d8ab1536c170536bc0d5c
Keith Bender <k.bender30@gmail.com>
2023-10-18 23:51:47 -0700

    started scraper and added functionality to searchbar
    Signed-off-by: Keith Bender <k.bender30@gmail.com>

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,51 @@
 "use client"
 
+import { useState } from 'react'
+import { scrapeAndStoreProduct } from '@/lib/actions'
+
+const isValidAmazonProductUrl = (url: string) => {
+    try{
+        const parsedUrl = new URL(url);
+        const hostname = parsedUrl.hostname;
+
+        if (
+            hostname.includes('amazon.com') || 
+            hostname.includes('amazon.') || 
+            hostname.endsWith('amazon')
+        ) {
+            return true
+        }
+    } catch (error){
+        return false
+    }
+
+    return false
+
+}
+
+
 const Searchbar = () => {
-    const handleSubmit = () => {}
+    const [searchPrompt, setSearchPrompt] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = async ( event: FormEvent<HTMLFormElement> ) => {
+        event.preventDefault();
+
+        const isValidLink = isValidAmazonProductUrl(searchPrompt);
+
+        if(!isValidLink) return alert('Please provide a valid Amazon link');
+
+        try {
+            setIsLoading(true);
+
+            const product = await scrapeAndStoreProduct(searchPrompt);
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     return (
         <form 
@@ -10,12 +54,18 @@ const Searchbar = () => {
         >
             <input 
                 type="text"
+                value={searchPrompt}
+                onChange={(e) => setSearchPrompt(e.target.value)}
                 placeholder="Enter product link"
                 className="searchbar-input"
             />
 
-            <button type="submit" className="searchbar-btn">
-                Search
+        <button 
+            type="submit" 
+            className="searchbar-btn"
+            disabled={searchPrompt === ''}
+        >
+                {isLoading ? 'Searching...' : 'Search'}
             </button>
         </form>
     )
